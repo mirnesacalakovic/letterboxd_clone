e51dcf831df3a3e2410c799ff39972a2f360189f
programerela <curicela@gmail.com>
2026-09-02 11:30:49 +0200

    switft init

---
+++ frontend/letterboxd/letterboxd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  letterboxd
+//
+//  Created by Mirnesa Calakovic on 2. 9. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frontend/letterboxd/letterboxd/letterboxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  letterboxdApp.swift
+//  letterboxd
+//
+//  Created by Mirnesa Calakovic on 2. 9. 2026..
+//
+
+import SwiftUI
+
+@main
+struct letterboxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
